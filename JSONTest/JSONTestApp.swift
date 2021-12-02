b64b8d89bbe19e1250a8bebc11e404f756557248
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-02 16:30:59 +0200

    Initial Commit

---
+++ JSONTest/JSONTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONTestApp.swift
+//  JSONTest
+//
+//  Created by Vahtee Boo on 02.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct JSONTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
