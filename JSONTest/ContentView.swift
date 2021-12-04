7f41623e7447779140fd26d429ad82731ca3e3fb
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-04 17:51:58 +0200

    Sheat

--- JSONTest/ContentView.swift
+++ JSONTest/ContentView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var posts: [Post] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(posts) { post in
+                VStack {
+                    Text(post.title)
+                        .fontWeight(.bold)
+                    Text(post.body)
+                }
+            }
+            .onAppear() {
+                Api().getPost { (posts) in
+                    self.posts = posts
+                }
+            }
+            .navigationBarTitle("Posts")
+        }
+    }
+}
+
+struct Post: Codable, Identifiable {
+    var id = UUID()
+    var title: String
+    var body: String
+}
+
+class Api {
+    func getPost(completion: @escaping ([Post]) -> ()) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            let posts = try! JSONDecoder().decode([Post].self, from: data!)
+            DispatchQueue.main.async {
+                completion(posts)
+            }
+        }
+        .resume()
     }
 }
 
